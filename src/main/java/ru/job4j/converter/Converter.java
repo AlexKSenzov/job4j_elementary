abc7a5d2887b5ab2bba5b722f6ee9625d0c82bb4
79992 <Leha_ksenzov@mail.ru>
2025-05-15 17:31:57 +0300

    1. Модульные тесты [#505277]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -1,9 +1,11 @@
 package ru.job4j.converter;
 
 public class Converter {
+
     public static double rubleToEuro(double value) {
         return value / 90; // Assuming the cinversion rate is 1 Euro = 90 Rybles
     }
+
     public static double rubleToDollar(double value) {
         return value / 75; // Assuming the cinversion rate is 1 Dollar = 75 Rubles
     }
