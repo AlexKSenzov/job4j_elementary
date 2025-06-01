15e2103e6f35ac6db65879b722fcafe701cedc8c
79992 <Leha_ksenzov@mail.ru>
2025-06-01 17:42:37 +0300

    Сумма чисел [#505208]

---
+++ src/test/java/ru/job4j/loop/CounterTest.java
@@ -0,0 +1,46 @@
+package ru.job4j.loop;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class CounterTest {
+
+    @Test
+    void whenStart0Finish10Sum55() {
+        int start = 0;
+        int finish = 10;
+        int result = Counter.sum(start, finish);
+        int expected = 55;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenStart3Finish8Sum33() {
+        int start = 3;
+        int finish = 8;
+        int result = Counter.sum(start, finish);
+        int expected = 33;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenStart1Finish1Sum1() {
+        int start = 1;
+        int finish = 1;
+        int result = Counter.sum(start, finish);
+        int expected = 1;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenStart0Finish1Sum0() {
+        int start = 11;
+        int finish = 7;
+        int result = Counter.sum(start, finish);
+        int expected = 0;
+        assertThat(result).isEqualTo(expected);
+    }
+
+}
+
